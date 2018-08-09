2f239f7e7bb6458c637edac20426d89ede285727
dragonfruit <maoh93@live.com>
2018-08-10 01:23:29 +0200

    Learn Spring Boot tutorial

---
+++ mySqlStudentDao.java
@@ -0,0 +1,40 @@
+package com.yourname.Dao;
+
+import com.yourname.Entity.Student;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+@Repository
+@Qualifier("mySQLData")
+
+public class mySqlStudentDao implements studentDao {
+
+
+
+    @Override
+    public Collection<Student> getAllStudents() {
+        return null;
+    }
+
+    @Override
+    public Student getStudentById(int id) {
+        return null;
+    }
+
+    @Override
+    public void removeStudentById(int id) {
+
+    }
+
+    @Override
+    public void updateStudent(Student student) {
+
+    }
+
+    @Override
+    public void insertStudentToDb(Student student) {
+
+    }
+}
